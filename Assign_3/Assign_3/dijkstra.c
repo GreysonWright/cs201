41c7f6488557440a05994738d141d631c07b9138
Greyson Wright <greysonmwright@gmail.com>
2017-03-22 01:55:57 -0500

    #assign3 - cleaned up main

--- Assign_3/Assign_3/dijkstra.c
+++ Assign_3/Assign_3/dijkstra.c
@@ -14,21 +14,12 @@
 int main(int argc, const char * argv[]) {
 	Binomial *binHeap = newBinomial(displayInteger, compareInteger, 0);
 	Graph *graph = newGraph(10, displayInteger);
-//	insertGraph(graph, 9, 2, newInteger(10));
+	
 	for (int i = 0; i < sizeGraph(graph); i++) {
 		for (int j = 0; j < sizeGraph(graph); j++) {
-			insertGraph(graph, i, j, newInteger(i + j));
+			insertBinomial(binHeap, getGraph(graph, i, j));
 		}
 	}
-//
-	insertGraph(graph, 10, 10, newInteger(4));
-	displayGraph(stdout, graph);
-	
-//	for (int i = 0; i < capacityGraph(graph); i++) {
-//		for (int j = 0; j < capacityGraph(graph); j++) {
-//			insertBinomial(binHeap, newInteger(getGraph(graph, i, j)));
-//		}
-//	}
 	
     return 0;
 }
