e000997e227070a0e4d4d49f65e22413f456e9f2
Greyson Wright <greysonmwright@gmail.com>
2017-03-22 01:56:40 -0500

    #assign3 - cleaned up graph

--- Assign_3/Assign_3/graph.c
+++ Assign_3/Assign_3/graph.c
@@ -20,47 +20,10 @@ int max(int a, int b) {
 	return a > b ? a : b;
 }
 
-//int **initMatrix(int capacity) {
-//	int **matrix = malloc((sizeof *matrix) * capacity);
-//	for (int i = 0; i < capacity; i++) {
-//		matrix[i] = malloc((sizeof **matrix) * capacity);
-//	}
-//	
-//	for (int i = 0; i < capacity; i++) {
-//		for (int j = 0; j < capacity; j++) {
-//			matrix[i][j] = 0;
-//		}
-//	}
-//	return matrix;
-//}
-
-//void growMatrix(int **matrix, int oldcapacity, int newcapacity) {
-//	matrix = realloc(matrix, (sizeof *matrix) * newcapacity);
-//	for (int i = 0; i < oldcapacity; i++) {
-//		matrix[i] = realloc(matrix[i], (sizeof **matrix) * newcapacity);
-//	}
-//	
-//	for (int i = oldcapacity; i < newcapacity; i++) {
-//		matrix[i] = malloc((sizeof **matrix) * newcapacity);
-//	}
-//	
-//	for (int i = oldcapacity; i < newcapacity; i++) {
-//		for (int j = 0; j < newcapacity; j++) {
-//			matrix[i][j] = 0;
-//		}
-//	}
-//	
-//	for (int i = 0; i < oldcapacity; i++) {
-//		for (int j = oldcapacity; j < newcapacity; j++) {
-//			matrix[i][j] = 0;
-//		}
-//	}
-//}
-
 Graph *newGraph(int size, void (*display)(FILE *, void *)) {
 	Graph *graph = malloc(sizeof *graph);
 	graph->display = display;
-	graph->matrix = newDArray(display);//initMatrix(capacity);
+	graph->matrix = newDArray(display);
 	
 	for (int i = 0; i < size; i++) {
 		setDArray(graph->matrix, i, newDArray(display));
@@ -75,12 +38,6 @@ Graph *newGraph(int size, void (*display)(FILE *, void *)) {
 }
 
 void insertGraph(Graph *graph, int e1, int e2, void *weight) {
-//	int largestEdge = max(e1, e2);
-//	if (largestEdge > graph->capacity) {
-//		growMatrix(graph->matrix, graph->capacity, largestEdge * 2);
-//		graph->capacity = largestEdge * 2;
-//	}
-//	graph->matrix[e1][e2] = weight;
 	int oldSize = sizeDArray(graph->matrix);
 	int size = max(e1, e2) + 1;
 	if (size > oldSize) {
@@ -100,13 +57,9 @@ void *removeGraph(Graph* graph, int e1, int e2) {
 	void *value = getDArray(getDArray(graph->matrix, e2), e1);
 	setDArray(getDArray(graph->matrix, e1), e2, newInteger(0));
 	return value;
-//	int weight = graph->matrix[e1][e2];
-//	graph->matrix[e1][e2] = 0;
-//	return weight;
 }
 
 void *getGraph(Graph *graph, int e1, int e2) {
-//	return graph->matrix[e1][e2];
 	return getDArray(getDArray(graph->matrix, e1), e2);
 }
 
