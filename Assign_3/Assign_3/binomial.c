4b3d6401c914754fbd44d047d4cef29ac54ded42
Greyson Wright <greysonmwright@gmail.com>
2017-04-01 23:18:04 -0500

    #assign3 - made bin heap display conform to spec

--- Assign_3/Assign_3/binomial.c
+++ Assign_3/Assign_3/binomial.c
@@ -204,7 +204,7 @@ void displayBinomial(FILE *file, Binomial *binHeap) {
 	queue *queueItems = newQueue(binHeap->display);
 	int count = 0;
 	
-	fprintf(file, "%d :", count++);
+	fprintf(file, "%d:", count++);
 	if (sizeBinomial(binHeap) == 0) {
 		fprintf(file, "\n----");
 	}
@@ -222,7 +222,7 @@ void displayBinomial(FILE *file, Binomial *binHeap) {
 			if (node == 0) {
 				node = dequeue(queueItems);
 				fprintf(file, "\n");
-				fprintf(file, "%d : ", count++);
+				fprintf(file, "%d: ", count++);
 				enqueue(queueItems, 0);
 			}
 			
